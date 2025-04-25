b61567a0adb1c781e99f0b744db63de0ff22afd9
olili27 <timmolili27@gmail.com>
2025-04-25 17:39:06 +0300

    finalize with testing signin feature

--- tests/steps_tests/test_signin.py
+++ tests/steps_tests/test_signin.py
@@ -34,6 +34,7 @@ def enter_password():
     print("enter password...")
 
 
+@when("I click send signin code button")
 @when("I click the signin button")
 def click():
     print("click signin button...")
@@ -59,7 +60,6 @@ def on_browse_page():
 
 #  Unsuccessful Signin with a correct email and incorrect password
 
-
 @when("I enter a correct email")
 def enter_email():
     print("enter email...")
@@ -70,18 +70,40 @@ def enter_password():
     print("enter password...")
 
 
-@when("I click the signin button")
-def click():
-    print("click signin button...")
-
-
 @then("I should remain on the signin page")
 def stay_on_signin_page():
     print("stay on signin page...")
 
 
 @then(
-    "I should see a message `Incorrect password for {correct email}. You can use a sign-in code, reset your password or try again`."
+    "I should see a message `Incorrect password for {correct email}. You can use a sign-in code, reset your password or try again`"
 )
 def see_message():
     print("see message...")
+
+
+# Successful Signin with a correct email and correct and valid signin code
+
+@when("I enter a correct email")
+def enter_email():
+    print("enter email...")
+
+
+@when("I click the signin button")
+def click():
+    print("click signin button...")
+
+
+@then("I should receive an email with a signin code")
+def receive_email():
+    print("receive signin code via email...")
+
+
+@then("I enter the correct and valid signin code that was sent")
+def enter_signin_code():
+    print("enter signin code...")
+
+
+@then("I am redirected to the browse page")
+def receive_email():
+    print("receive signin code via email...")
